fb15628b5b2b46bac8708c467b0a971393a921c7
HeleneAmouzou <helene.amouzou@gmail.com>
2023-12-01 14:03:17 +0100

    Add domain and token to surge command

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Introduction GitHub Actions</h1>
+      <h2>Hello les elfes !</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
